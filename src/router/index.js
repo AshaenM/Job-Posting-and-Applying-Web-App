e0fe3beba5233c03a7590fcf24b4e0ff4237db84
AshaenM <ashaenmanuel41@gmail.com>
2025-05-18 14:59:43 +1000

    Added Apply form and job detail components to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import About from '../components/About.vue'
-import News from '../components/News.vue'
+import Jobs from '../components/Jobs.vue'
+import JobDetails from '../components/JobDetails.vue';
+import ApplyForm from '../components/ApplyForm.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
-    { path: '/news', component: News }
+    { path: '/jobs', component: Jobs },
+    { path: '/job/:id', name: 'JobDetails', component: JobDetails },
+    { path: '/apply/:id', name: 'ApplyForm', component: ApplyForm }
 ]
 const router = createRouter({
     history: createWebHistory(),
